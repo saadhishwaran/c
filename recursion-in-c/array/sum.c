d680b7feebf671859d595f34048ceffc7fff1b51
adhishwaran <aadhishwaran34@gmail.com>
2025-05-22 11:45:56 +0530

    new

---
+++ recursion-in-c/array/sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int sum(char arr[],int i){
+    
+}
+
+int main(){
+    char arr[] = {1,1,2,2,3};
+    sum(arr,0);
+}
---
+++ recursion-in-c/number/power-n.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int n,m;
+    printf("enter a no for power : ");
+    scanf("%d",&n);
+    printf("enter a no for base : ");
+    scanf("%d",&m);
+    int x = power_m();
+}
