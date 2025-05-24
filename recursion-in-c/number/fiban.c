126fc14a04fb5f61571b7bf5b906bbeb6cec7385
adhishwaran <aadhishwaran34@gmail.com>
2025-05-24 11:23:17 +0530

    tarrif

--- recursion-in-c/number/fiban.c
+++ recursion-in-c/number/fiban.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+
 int fiban(int x){
-    for ( x>0;x+=1;x++){
-        
-    }
-    
-    
+    if (x==0) return 0;
+    if (x==1) return 1;
+    return fiban(x-1) + fiban(x-2);
 }
+
 int main(){
     int n;
     printf("enter n : ");
     scanf("%d",&n);
-    int x = fiban(n);
-    printf("%d",x);
+    for(int i=0; i<n; i++)
+        printf("%d ", fiban(i));
 }
