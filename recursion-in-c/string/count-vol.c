deeb76376520d5c876f2a6a0639461a1ef7d1d2f
adhishwaran <aadhishwaran34@gmail.com>
2025-05-26 11:52:42 +0530

    up

--- recursion-in-c/string/count-vol.c
+++ recursion-in-c/string/count-vol.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+int countvol(char str[],int ind){
+    if (str[ind] == '\0') return 0;
+    return ((str[ind] == 'a' || str[ind]=='e' || str[ind] =='i' || str[ind] == 'o' || str[ind] == 'u') + countvol(str,ind+1));
+}
+
 int main(){
-    
+    char str[100];
+    printf("enter the string ");
+    scanf("%s",str);
+    int n = countvol(str,0);
+    printf("the no of vol is %d",n);
 }
